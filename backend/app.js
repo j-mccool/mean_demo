06ea24a71ee9df1d72df3e1d47857aba42e94f73
John McCool <john.mccool@gmail.com>
2018-09-14 11:49:16 -0500

    more back end progress

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+  next();
+});
+
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+    id: "qowierlkansd31231",
+    title: "First server-side post",
+    contnent: "this is coming from the server"
+    },
+    {
+      id: "kasdlkfjalkwwer23",
+      title: "Second server-side post",
+      content: "It's also coming from the server!!!"
+    }
+  ];
+  res.status(200).json({
+    message: 'Posts fetched successfully!',
+    posts: posts
+  });
+});
+
+module.exports = app;
